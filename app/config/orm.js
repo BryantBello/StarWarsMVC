6a8784bf8c1db4adb55529e77466384e2a7093e9
bryantbello <bryant.bello@outlook.com>
2016-05-05 18:50:55 -0400

    linted

--- app/config/orm.js
+++ app/config/orm.js
@@ -13,49 +13,49 @@ var tableName = "allcharacters";
 
 var orm = {
 
-	// Here our ORM is creating a simple method for performing a query of the entire table.
-	// We make use of the callback to ensure that data is returned only once the query is done.
-	allCharacters: function(callback){
-		var s = 'SELECT * FROM ' + tableName;
+    // Here our ORM is creating a simple method for performing a query of the entire table.
+    // We make use of the callback to ensure that data is returned only once the query is done.
+    allCharacters: function(callback) {
+        var s = 'SELECT * FROM ' + tableName;
 
-		connection.query(s, function(err, result) {
-	 
+        connection.query(s, function(err, result) {
+
             callback(result);
 
         });
-	},
+    },
 
-	// Here our ORM is creating a simple method for performing a query of a single character in the table.
-	// Again, we make use of the callback to grab a specific character from the database. 
+    // Here our ORM is creating a simple method for performing a query of a single character in the table.
+    // Again, we make use of the callback to grab a specific character from the database. 
 
-	searchCharacter: function(name, callback){
-		var s = 'select * from ' + tableName + ' where routeName=?';
+    searchCharacter: function(name, callback) {
+        var s = 'select * from ' + tableName + ' where routeName=?';
 
-		connection.query(s,[name], function(err, result) {
-	 
+        connection.query(s, [name], function(err, result) {
+
             callback(result);
         });
 
-	},
+    },
 
-	// Here our ORM is creating a simple method for adding characters to the database
-	// Effectively, the ORM's simple addCharacter method translates into a more complex SQL INSERT statement. 
+    // Here our ORM is creating a simple method for adding characters to the database
+    // Effectively, the ORM's simple addCharacter method translates into a more complex SQL INSERT statement. 
 
-	addCharacter: function(character, callback){
+    addCharacter: function(character, callback) {
 
-		// Creating a routeName so its easy to search. 
-		var routeName = character.name.replace(/\s+/g, '').toLowerCase();
-		console.log(routeName);
+        // Creating a routeName so its easy to search. 
+        var routeName = character.name.replace(/\s+/g, '').toLowerCase();
+        console.log(routeName);
 
-		var s = "INSERT INTO " + tableName + " (routeName, name, role, age, forcePoints) VALUES (?,?,?,?,?)";
+        var s = "INSERT INTO " + tableName + " (routeName, name, role, age, forcePoints) VALUES (?,?,?,?,?)";
 
-		connection.query(s,[routeName, character.name, character.role, character.age, character.forcePoints], function(err, result) {
-            
+        connection.query(s, [routeName, character.name, character.role, character.age, character.forcePoints], function(err, result) {
+
             callback(result);
 
         });
 
-	}
+    }
 
 
 };
